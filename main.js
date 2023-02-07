a77da7837b5a2cb5211ca8adde481663ae6af641
alexcapricorngmailcom <107393171+alexcapricorngmailcom@users.noreply.github.com>
2023-02-07 16:58:13 +0200

    update johny

--- main.js
+++ main.js
@@ -16,48 +16,108 @@
 
 // Это: [3, 5, 13]
 
-let fib = [0, 1];
+// let fib = [0, 1];
 
-function JohnyOniNaDerevyah(from, to) {
-    for (let i = 2; i <= 22; i++) {
-        fib[i] = fib[i-2] + fib[i-1];  
-    }
+// function JohnyOniNaDerevyah(from, to) {
+//     for (let i = 2; i <= 22; i++) {
+//         fib[i] = fib[i-2] + fib[i-1];  
+//     }
 
-    console.log(fib);
+//     // console.log(fib);
 
-    let interval = fib.slice(from, to + 1);
+//     let interval = fib.slice(from, to + 1);
 
-    console.log(interval);
+//     console.log(interval);
 
-    function isPrime(n) {
-        if (n <= 1) {
-            return false;
-        } else if (n === 2) {
-            return true;
-        } else if (n % 2 === 0) {
-            return false;
-        } else {
-            for (let i = 2; i < n; i++) {
-                if (n % i === 0) {
-                    return false;
-                }
-            }
-            return true;
-        }
-    }
+//     function isPrime(n) {
+//         if (n <= 1) {
+//             return false;
+//         } else if (n === 2) {
+//             return true;
+//         } else if (n % 2 === 0) {
+//             return false;
+//         } else {
+//             for (let i = 2; i < n; i++) {
+//                 if (n % i === 0) {
+//                     return false;
+//                 }
+//             }
+//             return true;
+//         }
+//     }
 
-    let result = interval.filter(el => isPrime(el) === true);
+//     let result = interval.filter(el => isPrime(el) === true);
 
-    console.log(result);
+//     // console.log(result);
 
-    result.forEach(function(el) {
-        setInterval(() => {
-            console.log(el);
-        }, 1000);
-    });
-}
+//     let counter = 0;
 
-JohnyOniNaDerevyah(0, 22);
+//     let timer = setInterval(() => {
+//         console.log(result[counter]);
+//         counter++;
+
+//         if (counter === result.length) {
+//             clearInterval(timer);
+//         }
+
+//     }, 1000);
+// }
+
+// JohnyOniNaDerevyah(0, 22);
+
+
+// let fib = [0, 1];
+
+// function JohnyOniNaDerevyah(from, to) {
+//     for (let i = 2; i <= 22; i++) {
+//         fib[i] = fib[i-2] + fib[i-1];  
+//     }
+
+//     // console.log(fib);
+
+//     let interval = fib.slice(from, to + 1);
+
+//     console.log(interval);
+
+//     function isPrime(n) {
+//         if (n <= 1) {
+//             return false;
+//         } else if (n === 2) {
+//             return true;
+//         } else if (n % 2 === 0) {
+//             return false;
+//         } else {
+//             for (let i = 2; i < n; i++) {
+//                 if (n % i === 0) {
+//                     return false;
+//                 }
+//             }
+//             return true;
+//         }
+//     }
+
+//     let result = interval.filter(el => isPrime(el) === true);
+
+//     console.log(result);
+
+//     let counter = 0;
+
+//     let timer = setTimeout(function tick() {
+
+//         console.log(result[counter]);
+//         counter++;
+
+//         timer = setTimeout(tick, 1000)
+        
+//         if (counter === result.length) {
+//             clearTimeout(timer);
+//         }
+        
+//     }, 1000);
+    
+// }
+
+// JohnyOniNaDerevyah(0, 22);
 
 
 
@@ -68,25 +128,37 @@ JohnyOniNaDerevyah(0, 22);
 
 // Результаты всех задач выводить в console.log(), ибо это неблокирующий таймеры способ вывода.
 
-// let result = '';
-// let i = 0;
+let result = '';
+let i = 0;
+let max = 100;
 
-// function pseudoLoader(t) {
-//     console.log('Console was cleared');
-//     let timer = setInterval(() => {
-//         console.clear();
-//         for (let i = 0; i < 10; i++) {
-//             result = result + '#'
-//         }
-//         i++;
-//         console.log(`0% ${result}`)
-//         if (i == 4) {
-//             clearInterval(timer);
-//             console.clear();
-//             console.log(`0% ${result} 100%`)
-//         }
+function pseudoLoader(t) {
+    console.log('Console was cleared');
+    let timer = setInterval(() => {
+        console.clear();
 
-//     }, t);
-// }
+        function getRandomIntInclusive(max) {
+            
+            return Math.floor(Math.random() * (max - 10 + 1) + 10);
+          }
 
-// pseudoLoader(1000);
+        max = getRandomIntInclusive(max);
+
+        for (let i = 0; i < getRandomIntInclusive(max); i++) {
+            result = result + '#'
+        }
+
+        i++;
+
+        console.log(`0% ${result}`)
+
+        if (i == 10) {
+            clearInterval(timer);
+            console.clear();
+            console.log(`0% ${result} 100%`)
+        }
+
+    }, t);  
+}
+
+pseudoLoader(1000);
